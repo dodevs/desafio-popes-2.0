66cc324525f4fd1410a9e12487317f851b397610
dodevs <douglass.sousa@outlook.com.br>
2019-10-16 12:35:29 -0300

    Adicionado exemplo de set e significado dos valores

--- express_server/snmp-test.js
+++ express_server/snmp-test.js
@@ -15,18 +15,27 @@ session.get({oid: [1, 3, 6, 1, 2, 1, 1, 1, 0]}, function(error, varbinds) {
     }
 );
 
-//porta 01
+
+//porta 01 set
+/*session.set({oid: [1,3,6,1,2,1,2,2,1,8,1001], value: 2, type: 2}, function(error, varbinds) {
+	if(error)
+		console.log('Fail');
+	else
+		console.log('Foi setado');
+});*/
+
+//porta 01 get
 session.get({oid: [1,3,6,1,2,1,2,2,1,8,1001]}, function(error, varbinds) {
 	if(error)
 		console.log('Fail');
 	else
-		console.log(`Porta 01: ${varbinds[0].value}`);
+		console.log(`Porta 01: (${varbinds[0].value}) ${varbinds[0].value == 1 ? 'ligada' : 'desligada'} (${varbinds[0].type})`);
 });
 
 //porta 02
-session.get({oid: [1,3,6,1,2,1,2,2,1,8,1002]}, function(error, varbinds) {
+session.getNext({oid: [1,3,6,1,2,1,2,2,1,8,1002]}, function(error, varbinds) {
 	if(error)
 		console.log('Fail');
 	else
-		console.log(`Porta 02: ${varbinds[0].value}`);
+		console.log(`Porta 02: (${varbinds[0].value}) ${varbinds[0].value == 1 ? 'ligada' : 'desligada'} (${varbinds[0].type})`);
 });
