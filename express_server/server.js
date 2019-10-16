9e36535134abe5d2c4cfb6aa8b1406d1e28fa1b5
dodevs <douglass.sousa@outlook.com.br>
2019-10-15 23:24:43 -0300

    express server base

---
+++ express_server/server.js
@@ -0,0 +1,17 @@
+const app = require('express')(), port = 5000;
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+// cria conexao com o mongo
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+// Executa apos a conexao com o mongo
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+    app.use(bodyParser.json());
+
+    app.listen(port);
+})
