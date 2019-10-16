aca273dfe60f36a2a7083a99b5116f751150d207
dodevs <douglass.sousa@outlook.com.br>
2019-10-16 00:43:58 -0300

    Finalizado script de testes do snmp

--- express_server/snmp-test.js
+++ express_server/snmp-test.js
@@ -3,14 +3,30 @@ var snmp = require('snmp-native');
 var session = new snmp.Session({
     host: '200.137.87.181',
     port: 161,
-    comunity: 'd3s4f10'
+    community: 'd3s4f10'
 })
 
-session.get({
-    oid: [1, 3, 6, 1, 2, 1, 1, 1, 0], function(error, varbinds) {
+// nome switch
+session.get({oid: [1, 3, 6, 1, 2, 1, 1, 1, 0]}, function(error, varbinds) {
         if (error)
             console.log('Fail');
         else
-            console.log(varbinds[0].oid + ' = ' + varbinds[0].value + ' (' + varbinds[0].type + ')');
+            console.log(`nome: ${varbinds[0].value}`);
     }
-})
+);
+
+//porta 01
+session.get({oid: [1,3,6,1,2,1,2,2,1,8,1001]}, function(error, varbinds) {
+	if(error)
+		console.log('Fail');
+	else
+		console.log(`Porta 01: ${varbinds[0].value}`);
+});
+
+//porta 02
+session.get({oid: [1,3,6,1,2,1,2,2,1,8,1002]}, function(error, varbinds) {
+	if(error)
+		console.log('Fail');
+	else
+		console.log(`Porta 02: ${varbinds[0].value}`);
+});
