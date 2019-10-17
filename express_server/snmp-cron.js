3b693b4bcf89c525c9ad9c990f6d090fdb7fda63
dodevs <douglass.sousa@outlook.com.br>
2019-10-17 00:34:35 -0300

    Corrigido erros no cron

--- express_server/snmp-cron.js
+++ express_server/snmp-cron.js
@@ -3,20 +3,18 @@ const cron = require("node-cron");
 const mongoose = require("mongoose");
 const Swit = mongoose.model("Switch");
 
-var oids = [
-    [1, 3, 6, 1, 2, 1, 1, 1, 0],
-    [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001],
-    [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002]
-];
+const nomeOid = [1, 3, 6, 1, 2, 1, 1, 1, 0];
+const porta01Oid = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001];
+const porta02Oid = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002];
 
 var oidsDict = {
-    [1, 3, 6, 1, 2, 1, 1, 1, 0]: "nome",
-    [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001]: "porta01",
-    [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002]: "porta02"
+    nomeOid: "nome",
+    porta01Oid: "porta01",
+    porta02Oid: "porta02"
 };
 
 module.exports = cron.schedule("*/1 * * * *", () => {
-    session.getAll({ oids: oids }, function (error, varbinds) {
+    session.getAll({ oids: [nomeOid, porta01Oid, porta02Oid] }, function (error, varbinds) {
         let switV = {};
         varbinds.forEach(function (vb) {
             switV[oidsDict[vb.oid]] = vb.value;
