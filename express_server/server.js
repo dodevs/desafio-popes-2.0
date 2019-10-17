869b3e78edd38c1e56540873075b2fe439e1630e
dodevs <douglass.sousa@outlook.com.br>
2019-10-17 00:25:58 -0300

    Adicionado job e rotas no server

--- express_server/server.js
+++ express_server/server.js
@@ -2,6 +2,7 @@ const app = require('express')(), port = 5000;
 
 const mongoose = require('mongoose');
 require('./api/models/swichModel');
+const snmpcron = require('./snmp-cron');
 
 const bodyParser = require('body-parser');
 
@@ -12,7 +13,12 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', () => {
+    snmpcron(); // Alimenta o banco de dados a cada 1 minuto
+
     app.use(bodyParser.json());
+
+    const routes = require('./api/routes/switchRouter');
+    routes(app);
 
     app.listen(port);
 })
