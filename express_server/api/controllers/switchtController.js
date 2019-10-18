4c3f1bc6dfe63ad06b4ca2096afa24644ac56716
dodevs <douglass.sousa@outlook.com.br>
2019-10-17 23:06:03 -0300

    Adicionado novos metodos http

--- express_server/api/controllers/switchtController.js
+++ express_server/api/controllers/switchtController.js
@@ -10,3 +10,28 @@ exports.switchList = function (req, res) {
         res.json(swit);
     });
 };
+
+exports.switchCreate = function (req, res) {
+    let newSwit = new Swit(req.body);
+    newSwit.save(function (err, swit) {
+        if (err)
+            res.send(err);
+        res.json(swit);
+    })
+}
+
+exports.switchUpdate = function (req, res) {
+    Swit.findByIdAndUpdate(req.body._id, req.body, function (err, swit) {
+        if (err)
+            res.send(err);
+        res.json(swit);
+    })
+}
+
+exports.switchDelete = function (req, res) {
+    Swit.findByIdAndDelete(req.body._id, function (err, swit) {
+        if (err)
+            res.send(err);
+        res.json(swit._id);
+    })
+}
--- express_server/api/routes/switchRouter.js
+++ express_server/api/routes/switchRouter.js
@@ -4,5 +4,8 @@ module.exports = function (app) {
     const switchController = require('../controllers/switchtController');
 
     app.route('/')
-        .get(switchController.switchList);
+        .get(switchController.switchList)
+        .post(switchController.switchCreate)
+        .put(switchController.switchUpdate)
+        .delete(switchController.switchDelete);
 }
