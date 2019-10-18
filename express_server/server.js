2f3b3a82030b41a5118513f6a899ae36a1bf826e
dodevs <douglass.sousa@outlook.com.br>
2019-10-17 23:05:40 -0300

    Modificado com nova estrutura

--- express_server/server.js
+++ express_server/server.js
@@ -1,10 +1,11 @@
 const app = require('express')(), port = 5000;
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 require('./api/models/switchModel');
-const snmpcron = require('./snmp-cron');
 
-const bodyParser = require('body-parser');
+// Executa chamada snmp de tempo em tempo, grava no banco de dados e emite por socket
+const snmplistener = require('./utils/snmp-listener');
 
 // cria conexao com o mongo
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
@@ -13,12 +14,13 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', () => {
-    snmpcron.start(); // Alimenta o banco de dados a cada 1 minuto
 
     app.use(bodyParser.json());
 
     const routes = require('./api/routes/switchRouter');
     routes(app);
 
-    app.listen(port);
+    const server = app.listen(port);
+
+    snmplistener(server);
 })
