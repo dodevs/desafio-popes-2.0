f0425530d81374d3ca68d0f9e0a267895cd4d383
dodevs <douglass.sousa@outlook.com.br>
2019-10-18 00:39:56 -0300

    Tentando implementacao com if

--- express_server/utils/snmp-get.js
+++ express_server/utils/snmp-get.js
@@ -1,5 +1,14 @@
 const snmp = require("snmp-native");
 
+const nomeOid = [1, 3, 6, 1, 2, 1, 1, 1, 0];
+const porta01Oid = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001];
+const porta02Oid = [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002];
+
+var oidsDict = {};
+oidsDict[nomeOid] = "nome";
+oidsDict[porta01Oid] = "porta01";
+oidsDict[porta02Oid] = "porta02";
+
 var session = new snmp.Session({
     host: '200.137.87.181',
     port: 161,
@@ -9,27 +18,21 @@ var session = new snmp.Session({
 module.exports.get = function() {
     return new Promise(
         function(resolve, reject) {
-            session.getAll({ oids: [[1, 3, 6, 1, 2, 1, 1, 1, 0], [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001], [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002]] }, function (error, varbinds) {
+            session.getAll({ oids: [nomeOid, porta01Oid, porta02Oid] }, function (error, varbinds) {
                 if(error)
                     reject(error);                
                 
-		let switV = {}
+                let switV = {}
 
                 varbinds.forEach(function (vb) {
-		    
-                    switch (vb.oid) {
-                        case [1, 3, 6, 1, 2, 1, 1, 1, 0]:
-                            switV['nome'] = vb.value;
-                            break;
-                        case [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001]:
-                            switV['porta01'] = vb.value == 1 ? 'Up' : 'Down';
-                            break;
-                        case [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1002]:
-                            switV['porta02'] = vb.value == 1 ? 'Up' : 'Down';
-                            break;
-                        default:
-                            break;
-                    }
+
+                    if(vb.oid == [1, 3, 6, 1, 2, 1, 1, 1, 0])
+                        switV['nome'] = vb.value;
+                    else if(vb.oid == [1, 3, 6, 1, 2, 1, 2, 2, 1, 8, 1001])
+                        switV['porta01'] = vb.value == 1 ? 'Up' : 'Down';
+                    else
+                        switV['porta02'] = vb.value == 1 ? 'Up' : 'Down';
+
                 })
 
                 resolve(switV);
